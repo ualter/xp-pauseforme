d64cf7d690eeef0a3027879e5ff32f18440d5345
Ualter Azambuja <ualter.junior@gmail.com>
2018-09-14 20:36:51 +0200

    .

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
+import { MapPage } from '../map/map';
+import { SettingsPage } from '../settings/settings';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = MapPage;
+  tab2Root = SettingsPage;
 
   constructor() {
 
